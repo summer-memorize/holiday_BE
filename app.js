12bcba94de5e5338fb0570d478621847c561703e
Jiyu Jang <apryll7995@gmail.com>
2023-10-06 09:29:40 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const morgan = require("morgan");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 8888;
+app.set("port", port);
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중", `\r\n http://localhost:${port}/ping`);
+});
