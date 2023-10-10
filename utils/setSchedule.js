a7b289fa1b8dc7a7f5de62145c67b0003d91b031
Jiyu Jang <apryll7995@gmail.com>
2023-10-10 23:40:34 +0900

    mongodb 저장 시 한국시간으로 저장되게 수정

--- utils/setSchedule.js
+++ utils/setSchedule.js
@@ -15,7 +15,11 @@ const updateHolidayInfoJob = () => {
         const year = parseInt(num / 10000);
         const month = parseInt((num % 10000) / 100);
         const day = parseInt(num % 100);
-        return new Date(year, month - 1, day);
+        const utcDate = new Date(year, month - 1, day);
+        const kstOffset = 9 * 60 * 60 * 1000;
+        const kstDate = new Date(utcDate.getTime() + kstOffset);
+
+        return kstDate;
       };
 
       const year = new Date().getFullYear();
